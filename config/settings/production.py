693577cbd5eab90055320c5d594a5d42d3f1becf
Chris Culhane <cfculhane@gmail.com>
2021-02-28 20:50:12 +1100

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,10 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "medusa.org.au"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=["localhost", "medusa.org.au", "medusa.pythonanywhere.com"],
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
