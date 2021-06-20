7897cc22965b7518ba2ae3bbf620b61fd706fda7
Chris Culhane <cfculhane@gmail.com>
2021-06-20 12:32:48 +1000

    Update post_commit.sh

--- post_commit.sh
+++ post_commit.sh
@@ -1,31 +1,28 @@
 # To run after a commit to the main branch
-function tc_progress() {
-    printf "%%steamcity[progressMessage '%%s']" '##' "$1"
-}
 
 # shellcheck disable=SC2164
 cd ~/medusa_website
 source ~/.virtualenvs/medusa_website/bin/activate
 echo
 
-tc_progress 'Fetching changes from Git ...'
+printf 'Fetching changes from Git ...'
 git pull && git checkout main
 echo
 
-tc_progress 'Updating Python dependencies ...'
+printf 'Updating Python dependencies ...'
 pip install -r requirements.txt
 echo
 
-#tc_progress 'Updating Node.js dependencies ...'
+#printf 'Updating Node.js dependencies ...'
 #npm install
 #echo
 
-tc_progress 'Collecting static files ...'
+printf 'Collecting static files ...'
 python manage.py collectstatic --noinput
 
-tc_progress 'Running migrations ...'
+printf 'Running migrations ...'
 python manage.py migrate
 
-tc_progress 'Restarting application ...'
+printf 'Restarting application ...'
 sudo systemctl restart gunicorn
 echo
