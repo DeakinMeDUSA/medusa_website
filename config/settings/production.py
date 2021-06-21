0443232934716291df593648682e046cc88fc546
Chris Culhane <cfculhane@gmail.com>
2021-06-21 22:36:57 +1000

    Updating sentry settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,7 @@ import logging
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 
 from .base import *  # noqa
 from .base import env
@@ -176,12 +177,13 @@ sentry_logging = LoggingIntegration(
     level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
     event_level=logging.WARNING,  # Send errors as events
 )
-integrations = [sentry_logging, DjangoIntegration()]
+integrations = [sentry_logging, DjangoIntegration(), RedisIntegration()]
 sentry_sdk.init(
     dsn=SENTRY_DSN,
     integrations=integrations,
     environment=env("SENTRY_ENVIRONMENT", default="production"),
-    traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", default=0.0),
+    traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE", default=1.0),
+
 )
 
 # Your stuff...
