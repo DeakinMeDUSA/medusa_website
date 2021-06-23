d2e4454c9396f923519eda9a3fc05215f3a41f71
chris360KS <cfculhane@gmail.com>
2021-06-24 07:15:47 +1000

    Adding more allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "medusa.pythonanywhere.com", "172.105.190.146",
-                 "cculhane.com"]
+                 "www.cculhane.com", "cculhane.com", "medusa.org.au"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
