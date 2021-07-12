c323afd2faf77153ccddd459540243bd9badcd2b
Chris Culhane <cfculhane@gmail.com>
2021-07-12 23:50:04 +1000

    Updating importer script

--- scripts/import_medusaq/import_medusq.py
+++ scripts/import_medusaq/import_medusq.py
@@ -2,6 +2,8 @@ import logging
 import os
 import sys
 
+from django.db import IntegrityError
+
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 import django
 
@@ -14,15 +16,15 @@ logger = logging.getLogger(__name__)
 import csv
 from pathlib import Path
 
-from django.db import IntegrityError
-
 from medusa_website.mcq_bank.models import Question, Answer, Category
 # IMPORT_FILE = Path(r"I:\GitHub\medusa_website\scripts\import_medusaq\McqData20201017034440.csv")
 from medusa_website.users.models import User
+from bs4 import UnicodeDammit
 
 user = User.objects.get(id=1)
 
-IMPORT_FILE = Path(r"I:\GitHub\medusa_website\scripts\import_medusaq\McqData_cleaned.csv")
+# IMPORT_FILE = Path(r"I:\GitHub\medusa_website\scripts\import_medusaq\McqData_cleaned.csv")
+IMPORT_FILE = Path("/home/medusa_it/medusa_website/scripts/import_medusaq/McqData_cleaned.csv")
 
 
 def parse_tf(tf: str):
@@ -35,7 +37,11 @@ def parse_tf(tf: str):
 
 
 rows = []
-with open(IMPORT_FILE, "r") as csvfile:
+
+damnit = UnicodeDammit(IMPORT_FILE.read_bytes())
+print(f"Detected encoding: {damnit.original_encoding}")
+# %%
+with open(IMPORT_FILE, "r", encoding=damnit.original_encoding) as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         rows.append(row)
@@ -57,7 +63,10 @@ for raw_q in questions_raw:
                         explanation=explanation,
                         is_flagged=parse_tf(raw_q["isflagged"])
                         )
-    question.save()
+    try:
+        question.save()
+    except IntegrityError:
+        continue
     correct_answer = Answer(text=raw_q["correctanswer"].strip(), correct=True, question=question)
     correct_answer.save()
     other_answers = [Answer(text=ans.strip(), correct=False, question=question
