f57eb9a9275061eacfc5449934603dfa18ef6a41
Chris Culhane <cfculhane@gmail.com>
2021-07-13 15:15:23 +1000

    Create deploy script without static collection [no-ci]

---
+++ post_commit_nostatic.sh
@@ -0,0 +1,26 @@
+# To run after a commit to the main branch
+
+# shellcheck disable=SC2164
+cd ~/medusa_website
+source ~/.virtualenvs/medusa_website/bin/activate
+echo
+
+printf 'Fetching changes from Git ...'
+git fetch --all
+git reset --hard origin/main
+echo
+
+printf 'Updating Python dependencies ...'
+pip install -r requirements_prod.txt
+echo
+
+printf 'Updating Node.js dependencies ...'
+npm install
+echo
+
+printf 'Running migrations ...'
+python manage.py migrate
+
+printf 'Restarting application ...'
+sudo systemctl restart gunicorn
+echo
