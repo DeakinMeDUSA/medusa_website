fb2e4090cd8e21e2db66605100795c2cf3e19664
Chris Culhane <cfculhane@gmail.com>
2021-07-14 16:51:53 +1000

    Move pages to their own views

--- medusa_website/pages/views.py
+++ medusa_website/pages/views.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 class HomeView(TemplateView):
     template_name = "pages/home.html"
 
-    def get_context_data(self, **kwargs):
-        user = self.request.user
-        logger.info(f"user = {user} | authenticated = {user.is_authenticated}")
-        return super(HomeView, self).get_context_data(**kwargs)
+
+class AboutView(TemplateView):
+    template_name = "pages/home.html"
