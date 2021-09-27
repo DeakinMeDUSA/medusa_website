b84028d700e2e88d06918de7a86f2232322c7ff6
Chris Culhane <cfculhane@gmail.com>
2021-09-27 13:32:10 +1000

    Shortening redis cache, disabling translations, attempting to disable caching of messages

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,7 +32,7 @@ LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
-USE_I18N = True
+USE_I18N = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
 USE_L10N = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
--- config/settings/production.py
+++ config/settings/production.py
@@ -35,11 +35,12 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         # "LOCATION": env("REDIS_URL"),
         "LOCATION": "redis://127.0.0.1:6379/1",
+        "TIMEOUT": 30,  #
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             # Mimicing memcache behavior.
             # https://github.com/jazzband/django-redis#memcached-exceptions-behavior
-            # "IGNORE_EXCEPTIONS": True,
+            "IGNORE_EXCEPTIONS": True,
         },
     }
 }
