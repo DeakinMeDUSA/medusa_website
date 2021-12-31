22c00248ef620ce596c7ced182b249ed639c6b63
Chris Culhane <cfculhane@gmail.com>
2021-12-31 13:15:01 +1100

    try to debug tasks on prod

--- scripts/env_config.sh
+++ scripts/env_config.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #set -euo pipefail # Exit on any non-zero exit code, and error on use of undefined var
 
 # Load .env file
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Using SCRIPT_DIR = $SCRIPT_DIR"
-export enable_trap_debug=1
-source "$SCRIPT_DIR/lib.trap.sh"
+if test -f "$HOME/tools/shell-core/base/trap.bash"; then
+  echo "Loading bash error handler..."
+  source "$HOME/tools/shell-core/base/trap.bash"
+fi
+
 PARENT_PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 
 export PATH="$HOME/.poetry/bin:$PATH"
