083a68e6a389cddb09bbf719abd38bd16a090917
Chris Culhane <cfculhane@gmail.com>
2022-01-13 10:43:42 +1100

    Add existing user validator

--- medusa_website/users/validators.py
+++ medusa_website/users/validators.py
@@ -4,6 +4,42 @@ from django.core.validators import EmailValidator
 from medusa_website.users.models import MemberRecord
 
 
+class ExistingUserValidator:
+    message = (
+        "The supplied email was found to match an existing user. \n"
+        "This user may have been generated automatically without a password, please visit"
+        "https://www.medusa.org.au/accounts/password/reset/ to set a new one."
+    )
+
+    code = "invalid"
+
+    def __init__(self, message=None, code=None, whitelist=None):
+        if message is not None:
+            self.message = message
+        if code is not None:
+            self.code = code
+        if whitelist is not None:
+            self.domain_whitelist = whitelist
+
+    def __call__(self, value):
+        if self.validate_existing_user_status(value) is False:
+            raise ValidationError(self.message, code=self.code)
+
+    def validate_existing_user_status(self, email: str):
+        try:
+            from medusa_website.users.models import User
+
+            User.objects.get(email=email.lower())
+            return False
+        except MemberRecord.DoesNotExist:
+            return True
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, MedusaMemberValidator) and (self.message == other.message) and (self.code == other.code)
+        )
+
+
 class MedusaMemberValidator:
     message = (
         "The supplied email was not found on member list supplied by DUSA, which is updated weekly. "
@@ -49,4 +85,4 @@ class MedusaMemberValidator:
         )
 
 
-CustomEmailValidator = [EmailValidator(), MedusaMemberValidator()]
+CustomEmailValidator = [EmailValidator(), MedusaMemberValidator(), ExistingUserValidator()]
